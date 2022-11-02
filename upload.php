54bfb1ab0d456e703de2b0cd4164839977561f1e
afsar-rabblesoft <114976939+afsar-rabblesoft@users.noreply.github.com>
2022-11-02 18:22:59 +0530

    pushing changes for view
    pushing changes for view and making code more readable

--- upload.php
+++ upload.php
@@ -48,35 +48,32 @@ class uploader{
            }
            
     }
+    function print2(){
+      $files = scandir("uploads");
+      for ($a = 2; $a < count($files); $a++)
+      {
+         ?>
+         <p>
+             <?php echo $files[$a]; ?>
+      
+             <a href="uploads/<?php echo $files[$a]; ?>" >
+                 View
+             </a>
+          
+         </p>
+         <?php
+
+         
+      }
+
+    }
    
    }
    if(isset($_FILES['image'])){
    
        $uploader   =   new uploader();
        $uploader->upload();
-      }
-      $files = scandir("uploads");
- 
-      // If you are using windows, first 2 indexes are "." and "..",
-      // if you are using Mac, you may need to start the loop from 3,
-      // because the 3rd index in Mac is ".DS_Store" (auto-generated file by Mac)
-      for ($a = 2; $a < count($files); $a++)
-      {
-          ?>
-          <p>
-              <!-- Displaying file name !-->
-              <?php echo $files[$a]; ?>
-       
-              <!-- href should be complete file path !-->
-              <!-- download attribute should be the name after it downloads !-->
-              <a href="uploads/<?php echo $files[$a]; ?>" >
-                  View
-              </a>
-              <!-- <a href="delete.php?name=uploads/<?php echo $files[$a]; ?>" style="color: red;">
-Delete
-</a> -->
-          </p>
-          <?php
+       $uploader->print2();
       }
       
 ?>
